57babeb7064e487d9194b9ea0e7fb0683976f076
mylesrankin <myles.rankin@sky.com>
2016-11-24 21:06:12 +0000

    Added BFS (plus Queue class) and fixed DFS + stack

--- week7-task1.py
+++ week7-task1.py
@@ -17,40 +17,71 @@ class Stack:
      def size(self):
          return len(self.items)
 
-class Graph(object):
-    def __init__(self):
+class Queue:
+     def __init__(self):
+          self.items = []
+
+     def isEmpty(self):
+          return self.items == []
+
+     def enqueue(self, item):
+          self.items.insert(0,item)
+
+     def dequeue(self):
+          return self.items.pop()
+
+     def size(self):
+          return len(self.items)
+
+class Graph:
+     def __init__(self):
         self.graphDict = {}
 
-    def addVertex(self,label):
-        if label not in self.graphDict:
-            self.graphDict[label] = []
-            return("Vertex created")
-        else:
-            return("Vertex exists")
+     def addVertex(self,label):
+          if label not in self.graphDict:
+               self.graphDict[label] = []
+               return("Vertex created")
+          else:
+                 return("Vertex exists")
 
-    def addEdge(self,vertex1,vertex2):
-        self.graphDict[vertex1].append(vertex2)
-        self.graphDict[vertex2].append(vertex1)
-        return("An edge has been created between",vertex1," and ",vertex2)
+     def addEdge(self,vertex1,vertex2):
+          self.graphDict[vertex1].append(vertex2)
+          self.graphDict[vertex2].append(vertex1)
+          return("An edge has been created between",vertex1," and ",vertex2)
 
-    def displayGraph(self):
-        return self.graphDict
+     def displayGraph(self):
+          return self.graphDict
 
-    def displayAdjacencyList(self):
-        for k,v in self.graphDict.items():
-            print(k , "|", v)
+     def displayAdjacencyList(self):
+          for k,v in self.graphDict.items():
+               print(k , "|", v)
 
-    def depthFirstSearch(self,startNode):
-        s = Stack()
-        visited = []
-        s.push(startNode)
-        while s.isEmpty() == False:
-            u = s.pop()
-            if u not in visited:
-                visited.append(u)
-                for e in self.graphDict[u]:
-                    s.push(e)
-        return visited
+     def depthFirstSearch(self,startNode):
+          s = Stack()
+          visited = []
+          s.push(startNode)
+          while s.isEmpty() == False:
+               u = s.pop()
+               if u not in visited:
+                    visited.append(u)
+                    for e in self.graphDict[u]:
+                         s.push(e)
+          return visited
+
+     def breadthFirstSearch(self,startVertex):
+          q = Queue()
+          visited = []
+          q.enqueue(startVertex)
+          while q.isEmpty() == False:
+               u = q.dequeue()
+               if u not in visited:
+                    visited.append(u)
+               for edge in self.graphDict[u]:
+                    if edge not in visited:
+                         q.enqueue(edge)
+          return visited
+          
+          
                 
 
 if __name__ == "__main__":
@@ -58,7 +89,13 @@ if __name__ == "__main__":
     g.addVertex("a")
     g.addVertex("b")
     g.addVertex("c")
+    g.addVertex("d")
+    g.addVertex("e")
     g.addEdge("a","b")
+    g.addEdge("a","e")
     g.addEdge("a","c")
+    g.addEdge("b","e")
+    g.addEdge("b","d")
     g.displayAdjacencyList()
     print(g.depthFirstSearch("a"))
+    print(g.breadthFirstSearch("a"))
