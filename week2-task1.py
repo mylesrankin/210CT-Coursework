e97deb1ac8dadf60af52a6f3b16b227bcb35bfe0
mylesrankin <myles.rankin@sky.com>
2016-12-02 13:30:39 +0000

    Fixed code to be the same as submission code.

--- week2-task1.py
+++ week2-task1.py
@@ -5,7 +5,9 @@ then squaring to find the highest square up to the input number
 '''
 
 def highestSquare(a):
-    b = a**(1/2) # root input
-    b = b-(b%1)  # take away remainder
-    b = b*b      # square to get highest perfect square
-    return b
+    if a >= 0:
+      b = a**(1/2) # root input
+      b = b-(b%1)  # take away remainder
+      return int(b*b)      # square to get highest perfect square
+    else:
+      return("Enter a number that is greater than zero")
